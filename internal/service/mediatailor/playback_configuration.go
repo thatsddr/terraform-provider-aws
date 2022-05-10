fadaef54ade4c063b7489fd0c85a9ac07d043114
thatsddr <domenico.diruocco@code.berlin>
2022-05-10 14:45:55 +0200

    feat: handle tag creation and update

--- internal/service/mediatailor/playback_configuration.go
+++ internal/service/mediatailor/playback_configuration.go
@@ -14,13 +14,15 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
+	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
+	"github.com/hashicorp/terraform-provider-aws/internal/verify"
 )
 
 func ResourcePlaybackConfiguration() *schema.Resource {
 	return &schema.Resource{
-		CreateContext: resourcePlaybackConfigurationPut,
+		CreateContext: resourcePlaybackConfigurationCreate,
 		ReadContext:   resourcePlaybackConfigurationRead,
-		UpdateContext: resourcePlaybackConfigurationPut,
+		UpdateContext: resourcePlaybackConfigurationUpdate,
 		DeleteContext: resourcePlaybackConfigurationDelete,
 		Importer: &schema.ResourceImporter{
 			StateContext: schema.ImportStatePassthroughContext,
@@ -178,6 +180,8 @@ func ResourcePlaybackConfiguration() *schema.Resource {
 				Type:     schema.TypeString,
 				Optional: true,
 			},
+			"tags":     tftags.TagsSchema(),
+			"tags_all": tftags.TagsSchemaComputed(),
 			"transcode_profile_name": {
 				Type:     schema.TypeString,
 				Optional: true,
@@ -188,118 +192,22 @@ func ResourcePlaybackConfiguration() *schema.Resource {
 				ValidateFunc: validation.StringLenBetween(1, 512),
 			},
 		},
-		CustomizeDiff: customdiff.All(
+		CustomizeDiff: customdiff.Sequence(
 			customdiff.ForceNewIfChange("name", func(ctx context.Context, old, new, meta interface{}) bool { return old.(string) != new.(string) }),
+			verify.SetTagsDiff,
 		),
 	}
 }
 
-func resourcePlaybackConfigurationPut(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+func resourcePlaybackConfigurationCreate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
 	conn := meta.(*conns.AWSClient).MediaTailorConn
+	defaultTagsConfig := meta.(*conns.AWSClient).DefaultTagsConfig
+	tags := defaultTagsConfig.MergeTags(tftags.New(d.Get("tags").(map[string]interface{})))
 
-	var params mediatailor.PutPlaybackConfigurationInput
+	var params = getPlaybackConfigurationInput(d)
 
-	if v, ok := d.GetOk("ad_decision_server_url"); ok {
-		params.AdDecisionServerUrl = aws.String(v.(string))
-	}
-
-	params.AvailSuppression = &mediatailor.AvailSuppression{}
-
-	if v, ok := d.GetOk("avail_suppression_mode"); ok && v != nil {
-		params.AvailSuppression.Mode = aws.String(v.(string))
-	}
-
-	if v, ok := d.GetOk("avail_suppression_value"); ok && v != nil {
-		params.AvailSuppression.Value = aws.String(v.(string))
-	}
-
-	if v, ok := d.GetOk("bumper"); ok && v.([]interface{})[0] != nil {
-		val := v.([]interface{})[0].(map[string]interface{})
-		temp := mediatailor.Bumper{}
-		if str, ok := val["end_url"]; ok {
-			temp.EndUrl = aws.String(str.(string))
-		}
-		if str, ok := val["start_url"]; ok {
-			temp.StartUrl = aws.String(str.(string))
-		}
-		params.Bumper = &temp
-	}
-
-	if v, ok := d.GetOk("cdn_configuration"); ok && v.([]interface{})[0] != nil {
-		val := v.([]interface{})[0].(map[string]interface{})
-		temp := mediatailor.CdnConfiguration{}
-		if str, ok := val["ad_segment_url_prefix"]; ok {
-			temp.AdSegmentUrlPrefix = aws.String(str.(string))
-		}
-		if str, ok := val["content_segment_url_prefix"]; ok {
-			temp.ContentSegmentUrlPrefix = aws.String(str.(string))
-		}
-		params.CdnConfiguration = &temp
-	}
-
-	params.DashConfiguration = &mediatailor.DashConfigurationForPut{}
-
-	if v, ok := d.GetOk("dash_mpd_location"); ok && v != nil {
-		params.DashConfiguration.MpdLocation = aws.String(v.(string))
-	}
-	if v, ok := d.GetOk("dash_origin_manifest_type"); ok && v != nil {
-		params.DashConfiguration.OriginManifestType = aws.String(v.(string))
-	}
-
-	if v, ok := d.GetOk("live_pre_roll_configuration"); ok && v.([]interface{})[0] != nil {
-		val := v.([]interface{})[0].(map[string]interface{})
-		temp := mediatailor.LivePreRollConfiguration{}
-		if str, ok := val["ad_decision_server_url"]; ok {
-			temp.AdDecisionServerUrl = aws.String(str.(string))
-		}
-		if integer, ok := val["max_duration_seconds"]; ok {
-			temp.MaxDurationSeconds = aws.Int64(int64(integer.(int)))
-		}
-		params.LivePreRollConfiguration = &temp
-	}
-
-	if v, ok := d.GetOk("manifest_processing_rules"); ok && v.([]interface{})[0] != nil {
-		val := v.([]interface{})[0].(map[string]interface{})
-		temp := mediatailor.ManifestProcessingRules{}
-		if v2, ok := val["ad_marker_passthrough"]; ok {
-			temp2 := mediatailor.AdMarkerPassthrough{}
-			val2 := v2.([]interface{})[0].(map[string]interface{})
-			if boolean, ok := val2["enabled"]; ok {
-				temp2.Enabled = aws.Bool(boolean.(bool))
-			}
-			temp.AdMarkerPassthrough = &temp2
-		}
-		params.ManifestProcessingRules = &temp
-	}
-
-	if v, ok := d.GetOk("name"); ok {
-		params.Name = aws.String(v.(string))
-	}
-
-	if v, ok := d.GetOk("personalization_threshold_seconds"); ok {
-		params.PersonalizationThresholdSeconds = aws.Int64(int64(v.(int)))
-	}
-
-	if v, ok := d.GetOk("slate_ad_url"); ok {
-		params.SlateAdUrl = aws.String(v.(string))
-	}
-
-	tempMap := map[string]*string{}
-	if v, ok := d.GetOk("tags"); ok {
-		val := v.(map[string]interface{})
-		for k, value := range val {
-			temp := value.(string)
-			tempMap[k] = &temp
-		}
-	}
-	params.Tags = tempMap
-
-	if v, ok := d.GetOk("transcode_profile_name"); ok {
-		params.TranscodeProfileName = aws.String(v.(string))
-	}
-
-	if v, ok := d.GetOk("video_content_source_url"); ok {
-		params.VideoContentSourceUrl = aws.String(v.(string))
+	if len(tags) > 0 {
+		params.Tags = Tags(tags.IgnoreAWS())
 	}
 
 	playbackConfiguration, err := conn.PutPlaybackConfiguration(&params)
@@ -312,8 +220,30 @@ func resourcePlaybackConfigurationPut(ctx context.Context, d *schema.ResourceDat
 	return resourcePlaybackConfigurationRead(ctx, d, meta)
 }
 
+func resourcePlaybackConfigurationUpdate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	conn := meta.(*conns.AWSClient).MediaTailorConn
+
+	if d.HasChange("tags_all") {
+		o, n := d.GetChange("tags_all")
+		if err := UpdateTags(conn, d.Get("playback_configuration_arn").(string), o, n); err != nil {
+			return diag.FromErr(fmt.Errorf("error updating tags: %w", err))
+		}
+	}
+
+	var params = getPlaybackConfigurationInput(d)
+	playbackConfiguration, err := conn.PutPlaybackConfiguration(&params)
+	if err != nil {
+		return diag.FromErr(fmt.Errorf("error while creating the playback configuration: %v", err))
+	}
+	d.SetId(aws.StringValue(playbackConfiguration.PlaybackConfigurationArn))
+
+	return resourcePlaybackConfigurationRead(ctx, d, meta)
+}
+
 func resourcePlaybackConfigurationRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
 	conn := meta.(*conns.AWSClient).MediaTailorConn
+	defaultTagsConfig := meta.(*conns.AWSClient).DefaultTagsConfig
+	ignoreTagsConfig := meta.(*conns.AWSClient).IgnoreTagsConfig
 
 	resourceName := d.Get("name").(string)
 	if len(resourceName) == 0 && len(d.Id()) > 0 {
@@ -405,6 +335,20 @@ func resourcePlaybackConfigurationRead(ctx context.Context, d *schema.ResourceDa
 	d.Set("playback_endpoint_prefix", res.PlaybackEndpointPrefix)
 	d.Set("session_initialization_endpoint_prefix", res.SessionInitializationEndpointPrefix)
 	d.Set("slate_ad_url", res.SlateAdUrl)
+
+	arn := aws.StringValue(res.PlaybackConfigurationArn)
+	tags, err := ListTags(conn, arn)
+	if err != nil {
+		return diag.FromErr(fmt.Errorf("error listing tags for media store container (%s): %s", arn, err))
+	}
+	tags = tags.IgnoreAWS().IgnoreConfig(ignoreTagsConfig)
+	//lintignore:AWSR002
+	if err := d.Set("tags", tags.RemoveDefaultConfig(defaultTagsConfig).Map()); err != nil {
+		return diag.FromErr(fmt.Errorf("error setting tags: %w", err))
+	}
+	if err := d.Set("tags_all", tags.Map()); err != nil {
+		return diag.FromErr(fmt.Errorf("error setting tags_all: %w", err))
+	}
 	d.Set("transcode_profile_name", res.TranscodeProfileName)
 	d.Set("video_content_source_url", res.VideoContentSourceUrl)
 
@@ -420,4 +364,88 @@ func resourcePlaybackConfigurationDelete(ctx context.Context, d *schema.Resource
 	}
 
 	return nil
+}
+
+func getPlaybackConfigurationInput(d *schema.ResourceData) mediatailor.PutPlaybackConfigurationInput {
+	var params mediatailor.PutPlaybackConfigurationInput
+
+	if v, ok := d.GetOk("ad_decision_server_url"); ok {
+		params.AdDecisionServerUrl = aws.String(v.(string))
+	}
+	params.AvailSuppression = &mediatailor.AvailSuppression{}
+	if v, ok := d.GetOk("avail_suppression_mode"); ok && v != nil {
+		params.AvailSuppression.Mode = aws.String(v.(string))
+	}
+	if v, ok := d.GetOk("avail_suppression_value"); ok && v != nil {
+		params.AvailSuppression.Value = aws.String(v.(string))
+	}
+	if v, ok := d.GetOk("bumper"); ok && v.([]interface{})[0] != nil {
+		val := v.([]interface{})[0].(map[string]interface{})
+		temp := mediatailor.Bumper{}
+		if str, ok := val["end_url"]; ok {
+			temp.EndUrl = aws.String(str.(string))
+		}
+		if str, ok := val["start_url"]; ok {
+			temp.StartUrl = aws.String(str.(string))
+		}
+		params.Bumper = &temp
+	}
+	if v, ok := d.GetOk("cdn_configuration"); ok && v.([]interface{})[0] != nil {
+		val := v.([]interface{})[0].(map[string]interface{})
+		temp := mediatailor.CdnConfiguration{}
+		if str, ok := val["ad_segment_url_prefix"]; ok {
+			temp.AdSegmentUrlPrefix = aws.String(str.(string))
+		}
+		if str, ok := val["content_segment_url_prefix"]; ok {
+			temp.ContentSegmentUrlPrefix = aws.String(str.(string))
+		}
+		params.CdnConfiguration = &temp
+	}
+	params.DashConfiguration = &mediatailor.DashConfigurationForPut{}
+	if v, ok := d.GetOk("dash_mpd_location"); ok && v != nil {
+		params.DashConfiguration.MpdLocation = aws.String(v.(string))
+	}
+	if v, ok := d.GetOk("dash_origin_manifest_type"); ok && v != nil {
+		params.DashConfiguration.OriginManifestType = aws.String(v.(string))
+	}
+	if v, ok := d.GetOk("live_pre_roll_configuration"); ok && v.([]interface{})[0] != nil {
+		val := v.([]interface{})[0].(map[string]interface{})
+		temp := mediatailor.LivePreRollConfiguration{}
+		if str, ok := val["ad_decision_server_url"]; ok {
+			temp.AdDecisionServerUrl = aws.String(str.(string))
+		}
+		if integer, ok := val["max_duration_seconds"]; ok {
+			temp.MaxDurationSeconds = aws.Int64(int64(integer.(int)))
+		}
+		params.LivePreRollConfiguration = &temp
+	}
+	if v, ok := d.GetOk("manifest_processing_rules"); ok && v.([]interface{})[0] != nil {
+		val := v.([]interface{})[0].(map[string]interface{})
+		temp := mediatailor.ManifestProcessingRules{}
+		if v2, ok := val["ad_marker_passthrough"]; ok {
+			temp2 := mediatailor.AdMarkerPassthrough{}
+			val2 := v2.([]interface{})[0].(map[string]interface{})
+			if boolean, ok := val2["enabled"]; ok {
+				temp2.Enabled = aws.Bool(boolean.(bool))
+			}
+			temp.AdMarkerPassthrough = &temp2
+		}
+		params.ManifestProcessingRules = &temp
+	}
+	if v, ok := d.GetOk("name"); ok {
+		params.Name = aws.String(v.(string))
+	}
+	if v, ok := d.GetOk("personalization_threshold_seconds"); ok {
+		params.PersonalizationThresholdSeconds = aws.Int64(int64(v.(int)))
+	}
+	if v, ok := d.GetOk("slate_ad_url"); ok {
+		params.SlateAdUrl = aws.String(v.(string))
+	}
+	if v, ok := d.GetOk("transcode_profile_name"); ok {
+		params.TranscodeProfileName = aws.String(v.(string))
+	}
+	if v, ok := d.GetOk("video_content_source_url"); ok {
+		params.VideoContentSourceUrl = aws.String(v.(string))
+	}
+	return params
 }
